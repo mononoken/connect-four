0d960c48940e65b609777ef22bc53a99750507d2
mononoken <ken.maeshima@pm.me>
2022-08-26 21:00:31 -0700

    Fix first Game#board test and write first version of Game#board

---
+++ lib/game.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Game
+  def board
+    "| | | | | | | |\n" \
+    "| | | | | | | |\n" \
+    "| | | | | | | |\n" \
+    "| | | | | | | |\n" \
+    "| | | | | | | |\n" \
+    "| | | | | | | |"
+  end
+end
